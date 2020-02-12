b6cdd216143294aadcc5de66a1490bfc7b08c47e
abde <dhifabderrahmen@hotmail.fr>
2020-02-12 01:18:11 +0100

    add about

---
+++ R/build.R
@@ -0,0 +1,8 @@
+# blogdown::new_site(theme = 'jpescador/hugo-future-imperfect')
+# blogdown::serve_site()
+# options(blogdown.ext = '.Rmd', blogdown.author = 'Abde')
+# if (file.exists('~/.Rprofile')) {
+#   base::sys.source('~/.Rprofile', envir = environment())
+# }
+# options(blogdown.author = 'Abde') 
+#blogdown::build_site()
